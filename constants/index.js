8c5fec7d5b65efd2b0e5f592168cbc69674034c9
Merylosh <136605101+Merylosh@users.noreply.github.com>
2023-07-20 04:55:16 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe22e6f8ab5bf2bee5aa621ae2ee0c3e944136143";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
